29272e195eaaabe48213f396074e3a0ee90c6ca4
Rafsan12 <rafsan.web.2022@gmail.com>
2024-07-08 15:15:17 +0600

    create a context for this project

--- src/App.jsx
+++ src/App.jsx
@@ -3,18 +3,21 @@ import MovieList from "./components/Cine/MovieList";
 import Footer from "./components/Footer/Footer";
 import Header from "./components/Header/Header";
 import Sidebar from "./components/Sidebar/Sidebar";
+import { MovieContext } from "./Context";
 
 function App() {
   return (
     <>
-      <Header />
-      <main>
-        <div className="container grid lg:grid-cols-[218px_1fr] gap-[3.5rem]">
-          <Sidebar />
-          <MovieList />
-        </div>
-      </main>
-      <Footer />
+      <MovieContext.Provider>
+        <Header />
+        <main>
+          <div className="container grid lg:grid-cols-[218px_1fr] gap-[3.5rem]">
+            <Sidebar />
+            <MovieList />
+          </div>
+        </main>
+        <Footer />
+      </MovieContext.Provider>
     </>
   );
 }
